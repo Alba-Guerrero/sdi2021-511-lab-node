8880d381060b76fe88d832fe2c3a31f9d9a7d347
Alba <uo266007@uniovi.es>
2021-04-04 10:47:24 +0200

    SDI-2021-511-8.2-Arquitectura para el acceso a datos

--- app.js
+++ app.js
@@ -10,12 +10,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+let gestorBD = require("./modules/gestorBD.js");
+gestorBD.init(app,mongo);
+
+
 //Variables
 app.set('port',8081);
 app.set('db','mongodb://admin:sdi@tiendamusica-shard-00-00.mwotb.mongodb.net:27017,tiendamusica-shard-00-01.mwotb.mongodb.net:27017,tiendamusica-shard-00-02.mwotb.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-12uxxv-shard-0&authSource=admin&retryWrites=true&w=majority');
 //Rutas/controladores por lógica
-require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app,swig,mongo); // (app, param1, param2, etc.)
+require("./routes/rusuarios.js")(app,swig,gestorBD); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app,swig,gestorBD); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app,swig); // (app, param1, param2, etc.)
 
 
